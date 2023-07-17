19b5a9076aace343b898baef6293210340723b4f
guilhermelima <guilhermelimaruby@gmail.com>
2023-07-17 14:19:48 -0300

    go

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ while (chute != numeroSecreto) {
     let chute = prompt('Escolha um número entre 1 e 10')
     if (chute == numeroSecreto) {
         break
+    } 
+    if (chute > numeroSecreto) {
+        alert('O número secreto é menor')
     } else {
-        if (chute > numeroSecreto) {
-            alert('O número secreto é menor')
-        } else {
-            alert('O número secreto é maior')
-        }
+        alert('O número secreto é maior')
     }
     tentativas++
 }
